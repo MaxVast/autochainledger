ff0a468e3bfd311f087d2d58661ee136b0a8c784
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2023-11-27 17:00:30 +0100

    Update deploy.js

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,14 +1,8 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
 
-  const erc20 = await hre.ethers.deployContract("CarMaintenanceLoyalty", ["ERC20LOYALTY", "ERC20OTO"]);
+  const erc20 = await hre.ethers.deployContract("CarMaintenanceLoyalty");
 
   await erc20.waitForDeployment();
 
@@ -24,9 +18,6 @@ async function main() {
     `CarMaintenanceBook contract with ${erc20.target} for interface and deployed to ${nft.target}`
   );
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
