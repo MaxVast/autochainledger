d87bc0515a46699aeb8ce0cc32c3470f742c397b
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2023-11-28 10:58:49 +0100

    Update CarMaintenanceBook.sol

--- backend/contracts/CarMaintenanceBook.sol
+++ backend/contracts/CarMaintenanceBook.sol
@@ -38,8 +38,8 @@ contract CarMaintenanceBook is ERC721, Ownable, IERC5192 {
 
     /// @notice Emitted when the token is claim.
     /// @dev If a token is claimed, this event should be emitted.
-    /// @param tokenId The identifier for a token.
-    event TokenClaimed(address indexed user, uint256 tokenId);
+    /// @param idToken The identifier for a token.
+    event TokenClaimed(address indexed user, uint256 idToken);
 
     event DistributorRegistered(address indexed DistributorAddress);
 
@@ -50,6 +50,11 @@ contract CarMaintenanceBook is ERC721, Ownable, IERC5192 {
 
     modifier IsTransferAllowed(uint256 _tokenId) {
         require(!tokenData[_tokenId].locked, "Token is locked");
+        _;
+    }
+
+    modifier tokenIsExists(uint256 _idToken) {
+        require(claimedTokens[_idToken], "Token not exists");
         _;
     }
 
@@ -84,63 +89,50 @@ contract CarMaintenanceBook is ERC721, Ownable, IERC5192 {
     /// @notice Returns the locking status of an Soulbound Token
     /// @dev SBTs assigned to zero address are considered invalid, and queries
     /// about them do throw.
-    /// @param _tokenId The identifier for an SBT.
-    function locked(uint256 _tokenId) external view returns (bool) {
-        require(ownerOf(_tokenId) != address(0));
-        return tokenData[_tokenId].locked;
+    /// @param _idToken The identifier for an SBT.
+    function locked(uint256 _idToken) external view tokenIsExists(_idToken) returns  (bool)  {
+        return tokenData[_idToken].locked;
     }
 
-    function unlockToken(uint256 _tokenId) public onlyDistributor {
-        require(ownerOf(_tokenId) != address(0), "Token not exists");
-        tokenData[_tokenId].locked = false;
-        emit Unlocked(_tokenId);
+    function unlockToken(uint256 _idToken) public onlyDistributor tokenIsExists(_idToken) {
+        tokenData[_idToken].locked = false;
+        emit Unlocked(_idToken);
     }
 
-    function reclaimToken(address _from, uint256 _tokenId) external onlyDistributor {
-        require(ownerOf(_tokenId) != address(0), "Token does not exist");
-        require(ownerOf(_tokenId) == _from, "Token does not belong to the specified address");
-        _transfer(_from, msg.sender, _tokenId);
-        tokenData[_tokenId].locked = false;
+    function reclaimToken(address _from, uint256 _idToken) external onlyDistributor tokenIsExists(_idToken) {
+        require(ownerOf(_idToken) == _from, "Token does not belong to the specified address");
+        _transfer(_from, msg.sender, _idToken);
+        tokenData[_idToken].locked = false;
     }
 
-    function transferTokenNew(address _from, address _to, uint256 _tokenId) external onlyDistributor {
-        require(ownerOf(_tokenId) != address(0), "Token does not exist");
-        require(ownerOf(_tokenId) == _from, "Token does not belong to the specified address");
-        _transfer(_from, _to, _tokenId);
+    function transferTokenNew(address _from, address _to, uint256 _idToken) external onlyDistributor tokenIsExists(_idToken) {
+        require(ownerOf(_idToken) == _from, "Token does not belong to the specified address");
+        _transfer(_from, _to, _idToken);
     }
 
-    function getTokenURI(uint256 _tokenId) public view returns (string memory) {
-        require(ownerOf(_tokenId) != address(0), "Token does not exist");
-        return tokenData[_tokenId].uri;
+    function getTokenURI(uint256 _idToken) public view tokenIsExists(_idToken) returns (string memory) {
+        return tokenData[_idToken].uri;
     }
 
     function generateTokenId(string calldata _vin) public pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(_vin)));
     }
 
-    function addMaintenance(string calldata _maintenance, string calldata _vin) external onlyDistributor {
-        uint256 _idToken = generateTokenId(_vin);
-        require(ownerOf(_idToken) != address(0), "Token does not exist");
+    function addMaintenance(uint256 _idToken, string calldata _maintenance) external onlyDistributor tokenIsExists(_idToken) {
         Maintenances[_idToken].push(Maintenance(_maintenance, block.timestamp));
         // Credit 100 tokens to the cagnotte for each maintenance
         cagnotteToken.addCagnotte(ownerOf(_idToken), 100);
     }
 
-    function getMaintenanceHistory(string calldata _vin) external view returns (Maintenance[] memory) {
-        uint256 _idToken = generateTokenId(_vin);
-        require(ownerOf(_idToken) != address(0), "Token does not exist");
+    function getMaintenanceHistory(uint256 _idToken) external view tokenIsExists(_idToken) returns (Maintenance[] memory) {
         return Maintenances[_idToken];
     }
 
-    function getLengthMaintenanceHistory(string calldata _vin) external view returns (uint) {
-        uint256 _idToken = generateTokenId(_vin);
-        require(ownerOf(_idToken) != address(0), "Token does not exist");
+    function getLengthMaintenanceHistory(uint256 _idToken) external view tokenIsExists(_idToken) returns (uint) {
         return Maintenances[_idToken].length;
     }
 
-    function gethMaintenanceHistoryById(string calldata _vin, uint _idMaintenance) external view returns (Maintenance memory) {
-        uint256 _idToken = generateTokenId(_vin);
-        require(ownerOf(_idToken) != address(0), "Token does not exist");
+    function gethMaintenanceHistoryById(uint256 _idToken, uint _idMaintenance) external view tokenIsExists(_idToken) returns (Maintenance memory) {
         return Maintenances[_idToken][_idMaintenance];
     }
 
@@ -151,9 +143,5 @@ contract CarMaintenanceBook is ERC721, Ownable, IERC5192 {
 
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public override(ERC721) IsTransferAllowed(tokenId) {
         super.safeTransferFrom(from, to, tokenId, _data);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721) returns (bool) {
-        return super.supportsInterface(interfaceId);
     }
 }
