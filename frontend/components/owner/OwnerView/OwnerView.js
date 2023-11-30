3555272f6561028efaa447e85f772bb4b2648db4
Maxence Vast <34278027+MaxVast@users.noreply.github.com>
2023-11-30 14:57:30 +0100

    Update OwnerView.js

--- frontend/components/owner/OwnerView/OwnerView.js
+++ frontend/components/owner/OwnerView/OwnerView.js
@@ -2,7 +2,7 @@
 //ReactJS
 import { useState } from 'react'
 // Chackra UI
-import { ChatIcon, CopyIcon } from '@chakra-ui/icons'
+import { CopyIcon } from '@chakra-ui/icons'
 import { Flex, Heading, useToast, Input, Button, Text, VStack, Box, TableContainer, Table, Thead, Tr, Td, Th, Tbody, Center, Spacer, Badge } from '@chakra-ui/react'
 //Constants information SmartContract
 import { contractAbiCarMaintenanceBook, contractAddressCarMaintenanceBook, contractAbiCarMaintenanceLoyalty, contractAddressCarMaintenanceLoyalty } from '@/constants/index'
@@ -76,84 +76,84 @@ const OwnerView = () => {
     }
     return (
         <>
-            <Flex p="1rem" direction="column" width='100%'>
+            <Flex p="1rem" width='100%'>
                 <VStack spacing={10} height='100%' width='100%'>
                     <Box>
                         <Heading as='h4' size='md'>
-                            <ChatIcon marginRight='1rem' />Register Distributor address
+                            Register Distributor address
                         </Heading>
                     </Box>
 
-                    <Box width='100%' height='100%' paddingRight='2rem'>
+                    <Flex p="1rem" width='100%'>
+                        <Box width='50%' paddingRight='2rem'>
 
-                        <Flex mt="1rem">
-                            <Input type="text" value={distributor} onChange={e => setDistributor(e.target.value)} placeholder="enter a new voter address" fontSize='0.8rem' background='white'/>
-                            <Button onClick={() => addDistributor()} marginLeft='1rem'>
-                                <Text fontSize='0.8rem'>register</Text>
-                            </Button>
-                        </Flex>
-
-                    </Box>
-
-                    <Box w='100%'textAlign='left'>
-
-                        <Text as='b' paddingLeft='1rem' fontSize='sm' color='#3e3f42'>DISTRIBUTOR ({distributorCount})</Text>
-
-                        <Box w='100%' marginTop='0.5rem' paddingRight='2rem' overflow='scroll' maxHeight='20rem'>
-
-                            <TableContainer w='100%'>
-                                <Table size='sm' w='100%'>
-                                    <Tbody>
-                                    {
-
-                                        distributorCount > 0 ? distributorAddress.map((distributorAddress) => {
-
-                                            const shorten = distributorAddress;
-
-                                            return <Tr key={ shorten }>
-                                                    <Td>
-                                                        <Flex>
-                                                            <Center color='#444c5d' fontSize='0.8rem'>
-                                                                <Text as='samp'><b>{ getEthAddressStart(shorten) }</b></Text>
-                                                                <Text as='samp'>{ getEthAddressMiddle(shorten) }</Text>
-                                                                <Text as='samp'><b>{ getEthAddressEnd(shorten) }</b></Text>
-                                                                {
-                                                                    (shorten === ownerAddress) && (
-
-                                                                        <Badge colorScheme='purple' marginLeft='0.5rem'>Owner</Badge>
-
-                                                                    )
-                                                                }
-                                                            </Center>
-                                                            <Spacer />
-                                                            <Center>
-                                                                <Button marginLeft='0.25rem' size='xs' colorScheme='gray' variant='ghost' onClick={ () => copyToClipboard(distributorAddress) }>
-                                                                    <CopyIcon />
-                                                                </Button>
-                                                            </Center>
-                                                        </Flex>
-                                                    </Td>
-                                                </Tr>
-                                            
-
-                                        }) : (
-
-                                            <Tr>
-                                                <Td>
-                                                    <Center>
-                                                        <Text fontSize='0.9rem'>There is no registered distributor yet</Text>
-                                                    </Center>
-                                                </Td>
-                                            </Tr>
-                                        )
-                                    }
-                                    </Tbody>
-                                </Table>
-                            </TableContainer>
+                            <Flex mt="1rem">
+                                <Input type="text" value={distributor} onChange={e => setDistributor(e.target.value)} placeholder="enter a new voter address" fontSize='0.8rem' background='white'/>
+                                <Button onClick={() => addDistributor()} marginLeft='1rem'>
+                                    <Text fontSize='0.8rem'>register</Text>
+                                </Button>
+                            </Flex>
 
                         </Box>
 
-                    </Box>
+                        <Box w='50%' textAlign='left'>
+
+                            <Text as='b' paddingLeft='1rem' fontSize='sm' color='#3e3f42'>DISTRIBUTOR ({distributorCount})</Text>
+
+                            <Box w='100%' marginTop='0.5rem' paddingRight='2rem' overflow='scroll' maxHeight='20rem'>
+
+                                <TableContainer w='100%'>
+                                    <Table size='sm' w='100%'>
+                                        <Tbody>
+                                            {
+
+                                                distributorCount > 0 ? distributorAddress.map((distributorAddress) => {
+
+                                                    const shorten = distributorAddress;
+
+                                                    return <Tr key={ shorten }>
+                                                        <Td>
+                                                            <Flex>
+                                                                <Center color='#444c5d' fontSize='0.8rem'>
+                                                                    <Text as='samp'><b>{ getEthAddressStart(shorten) }</b></Text>
+                                                                    <Text as='samp'>{ getEthAddressMiddle(shorten) }</Text>
+                                                                    <Text as='samp'><b>{ getEthAddressEnd(shorten) }</b></Text>
+                                                                    {
+                                                                        (shorten === ownerAddress) && (
+
+                                                                            <Badge colorScheme='purple' marginLeft='0.5rem'>Owner</Badge>
+
+                                                                        )
+                                                                    }
+                                                                </Center>
+                                                                <Spacer />
+                                                                <Center>
+                                                                    <Button marginLeft='0.25rem' size='xs' colorScheme='gray' variant='ghost' onClick={ () => copyToClipboard(distributorAddress) }>
+                                                                        <CopyIcon />
+                                                                    </Button>
+                                                                </Center>
+                                                            </Flex>
+                                                        </Td>
+                                                    </Tr>
+
+
+                                                }) : (
+
+                                                    <Tr>
+                                                        <Td>
+                                                            <Center>
+                                                                <Text fontSize='0.9rem'>There is no registered distributor yet</Text>
+                                                            </Center>
+                                                        </Td>
+                                                    </Tr>
+                                                )
+                                            }
+                                        </Tbody>
+                                    </Table>
+                                </TableContainer>
+                            </Box>
+                        </Box>
+                    </Flex>
 
                 </VStack>
             </Flex>  
